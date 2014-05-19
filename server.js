ae0d74efb85d5cabf9415bb66c8a6bf7449e86b9
vpedro <vpcx5@yahoo.com.br>
2014-05-19 05:14:00 -0300

    Fix DB connection

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
     var mongoose = require('mongoose'); // mongoose for mongodb
 
     // configuration ========================
-    mongoose.connect('mongodb://vpcsilva:vpedro123@ds049198.mongolab.com:49198/heroku_app25394501'); 	// connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://todo-app:todo-app@ds049198.mongolab.com:49198/heroku_app25394501'); 	// connect to mongoDB database on modulus.io
 
     app.configure(function() {
             app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
