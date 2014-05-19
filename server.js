774c2299d701d3b8de74a2995511a24973cb1a9e
vpedro <vpcx5@yahoo.com.br>
2014-05-19 04:58:48 -0300

    Creating the Angular module and controller, following the tutorial

--- server.js
+++ server.js
@@ -13,6 +13,59 @@
             app.use(express.bodyParser()); 							// pull information from html in POST
         });
 
+    // Todo Model =================================
+    var Todo = mongoose.model('Todo', {
+    text : String
+    });
+
+    // routes =======================
+    app.get('/api/todos', function(req, res) {
+        Todo.find(function(err, todos) {
+            if(err)
+                res.send(err);
+            res.json(todos);
+        });
+    });
+
+    app.post('/api/todos', function(req, res) {
+        Todo.create({
+            text : req.body.text,
+            done : false,
+        },
+        function(err, todo) {
+            if(err)
+                res.send(err);
+            
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err);
+                res.json(todos);
+            });
+        });
+    });
+
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        },
+        function(err, todo) {
+            if (err)
+                res.send(err);
+            
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err);
+                
+                res.json(todos);
+            });
+        });
+    });
+
+    // Routing frontend
+    app.get('*', function(req, res){
+        res.sendfile('./public/index.html');
+    });
+
     // listen (start app with node server.js) ======================================
     app.listen(8080);
     console.log("App listening on port 8080");
